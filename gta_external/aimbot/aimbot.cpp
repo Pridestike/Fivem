b5d275988978da6fcc2c963605fd393c4c5bb5c3
Fnoberz <FnoberzOfficial@pm.me>
2022-09-25 04:41:33 +0700

    Update aimbot.cpp

--- gta_external/aimbot/aimbot.cpp
+++ gta_external/aimbot/aimbot.cpp
@@ -1,3 +1,32 @@
 #include "aimbot.hpp"
 #include "../esp/esp.hpp"
 
+static const char alphanum[] = "0123456789" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz"; // he has a brain atleast
+
+int stringLength1 = sizeof(alphanum) - 1;
+
+char genRandom1()
+{
+
+    return alphanum[rand() % stringLength1];
+}
+
+// Log a message in the console with a timestamp. LogType changes the color of the text. aka you're lazy fucker.
+void Log(std::string Message, int LogType)
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);;
+
+    SYSTEMTIME st, lt;
+
+    GetSystemTime(&st);
+    GetLocalTime(&lt);
+
+    SetConsoleTextAttribute(hConsole, 9);
+    printf("[%02d:%02d:%02d] ", st.wHour, st.wMinute, st.wSecond);
+
+    SetConsoleTextAttribute(hConsole, LogType);
+    std::cout << Message << std::endl;
+
+    SetConsoleTextAttribute(hConsole, 15);
+}
+
