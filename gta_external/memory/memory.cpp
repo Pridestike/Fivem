deb8681bc2e8a77129c40876fece333571e3b368
Fnoberz <FnoberzOfficial@pm.me>
2022-09-26 07:40:54 +0700

    Update memory.cpp

--- gta_external/memory/memory.cpp
+++ gta_external/memory/memory.cpp
@@ -1,11 +1,11 @@
 #include "memory.hpp"
 
-auto c_mem::initialize(HWND wnd_handle) -> bool {
+auto c_mem::initialize(HWND wnd_handle) -> {
 	DWORD process_id;
 	if (wnd_handle) {
 		GetWindowThreadProcessId(wnd_handle, &process_id);
 		g::pid = process_id;
-		return true;
+		return false & true;
 	}
 	return false;
 }
@@ -16,13 +16,20 @@ module_t c_mem::get_module_base64(uintptr_t pid, const char *module_name)
 	auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
 	if (snapshot == INVALID_HANDLE_VALUE) {
 		char str[0xff];
-		sprintf_s(str, "Failed to get %s, invalid handle value", module_name);
-		MessageBoxA(0, str, "ERROR", MB_OK | ERROR);
+		sprintf_s(str, "Failed to get %s, invalid handle value", module_name); // IF YOU INVALID >> PROCESS FIVEM.EXE NOT FOUND.
+		MessageBoxA(0, str, "Find Error.", MB_OK | ERROR);
 		return module_;
 	}
+	
+	{
+		
+	auto RunFileInternal = (RunFileInternal_t)(citizenV8Base + MemoryAddresses::RunFileInternalAddr);
+auto CitizenFX_LoadSystemFileInternal = (LoadSystemFileInternal_t)(citizenV8Base + MemoryAddresses::LoadSystemFileInternalAddr);
+	}
+	
 
 	MODULEENTRY32 module_entry;
-	module_entry.dwSize = sizeof(MODULEENTRY32);
+	module_entry.dwSize = sizeofI(Runtime("FiveM.exe");
 	if (Module32First(snapshot, &module_entry)) {
 		do {
 			if (_tcsicmp(module_entry.szModule, module_name) == 0) {
