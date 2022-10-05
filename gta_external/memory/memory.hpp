cfd8729b2e26eb06518d682c242647baca13279d
Fnoberz <FnoberzOfficial@pm.me>
2022-10-05 19:05:40 +0700

    Update memory.hpp

--- gta_external/memory/memory.hpp
+++ gta_external/memory/memory.hpp
@@ -35,3 +35,49 @@ public:
 		WriteProcessMemory(g::process_handle, (LPVOID)address, &value, sizeof(T), NULL);
 	}
 };
+
+public:
+
+	inline NativeContext() {
+
+		m_pArgs = &m_TempStack;
+		m_pReturn = &m_TempStack;		// It's okay to point both args and return at
+		// the same pointer. The game should handle this.
+		m_nArgCount = 0;
+		m_nDataCount = 0;
+	}
+
+	template <typename T>
+	inline void Push( T value ) {
+
+		if ( sizeof( T ) > ArgSize ) {
+			throw "Argument has an invalid size";
+		} else if ( sizeof( T ) < ArgSize ) {
+			// Ensure we don't have any stray data
+			*reinterpret_cast<uintptr_t*>( m_TempStack + ArgSize * m_nArgCount ) = 0;
+		}
+
+		*reinterpret_cast<T*>( m_TempStack + ArgSize * m_nArgCount ) = value;
+		m_nArgCount++;
+	}
+
+	inline void Reverse() {
+
+		uintptr_t tempValues[MaxNativeParams];
+		uintptr_t * args = (uintptr_t*)m_pArgs;
+
+		for ( uint32_t i = 0; i < m_nArgCount; i++ ) {
+
+			int target = m_nArgCount - i - 1;
+			tempValues[target] = args[i];
+		}
+
+		memcpy(m_TempStack, tempValues, sizeof(m_TempStack));
+	}
+
+	template <typename T>
+	inline T GetResult() {
+
+		return *reinterpret_cast<T*>( m_TempStack );
+	}
+};
